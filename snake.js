7a8216f24bc364f8c0cf4b4caa1f343f517dcad1
carebearlover <42905793+webdevlover@users.noreply.github.com>
2019-01-06 16:10:01 +0300

    Add files via upload

--- snake.js
+++ snake.js
@@ -1,8 +1,36 @@
-// ссылка на игру в сети интернет
-let url = 'https://webdevlover.github.io'
+/**
+ * This example demonstrates using HTML5 games with Telegram.
+ */
+/* eslint-disable no-console */
 
-// название игры (то, что указывали в BotFather)
-const gameName = "GFEsnakebot"
+const TOKEN = process.env.TELEGRAM_TOKEN || '607622185:AAHcO4EDZoZVjM1pW58uiV2j-j4Y-xpFju0';
+const gameName = process.env.TELEGRAM_GAMENAME || '607622185:AAHcO4EDZoZVjM1pW58uiV2j-j4Y-xpFju0';
+// Specify '0' to use ngrok i.e. localhost tunneling
+let url = process.env.URL || 'https://webdevlover.github.io';
+const port = process.env.PORT || 8080;
+
+const TelegramBot = require('../..');
+const express = require('express');
+const path = require('path');
+
+const bot = new TelegramBot(TOKEN, { polling: true });
+const app = express();
+
+// Basic configurations
+app.set('view engine', 'ejs');
+
+// Tunnel to localhost.
+// This is just for demo purposes.
+// In your application, you will be using a static URL, probably that
+// you paid for. :)
+if (url === '0') {
+  const ngrok = require('ngrok');
+  ngrok.connect(port, function onConnect(error, u) {
+    if (error) throw error;
+    url = u;
+    console.log(`Game tunneled at ${url}`);
+  });
+}
 
 // Matches /start
 bot.onText(/\/start/, function onPhotoText(msg) {
@@ -14,146 +42,12 @@ bot.on('callback_query', function onCallbackQuery(callbackQuery) {
   bot.answerCallbackQuery(callbackQuery.id, { url });
 });
 
-const cvs = document.getElementById("GFEsnakebot");
-const ctx = cvs.getContext("2d");
+// Render the HTML game
+app.get('/', function requestListener(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-// create the unit
-const box = 32;
-
-// load images
-
-const ground = new Image();
-ground.src = "img/ground.png";
-
-const foodImg = new Image();
-foodImg.src = "img/food.png";
-
-// create the snake
-
-let snake = [];
-
-snake[0] = {
-    x : 9 * box,
-    y : 10 * box
-};
-
-// create the food
-
-let food = {
-    x : Math.floor(Math.random()*17+1) * box,
-    y : Math.floor(Math.random()*15+3) * box
-}
-
-// create the score var
-
-let score = 0;
-
-//control the snake
-
-let d;
-
-document.addEventListener("keydown",direction);
-
-function direction(event){
-    let key = event.keyCode;
-    if( key == 37 && d != "RIGHT"){
-        d = "LEFT";
-    }else if(key == 38 && d != "DOWN"){
-        d = "UP";
-    }else if(key == 39 && d != "LEFT"){
-        d = "RIGHT";
-    }else if(key == 40 && d != "UP"){
-        d = "DOWN";
-    }
-}
-
-// cheack collision function
-function collision(head,array){
-    for(let i = 0; i < array.length; i++){
-        if(head.x == array[i].x && head.y == array[i].y){
-            return true;
-        }
-    }
-    return false;
-}
-
-// draw everything to the canvas
-
-function draw(){
-    
-    ctx.drawImage(ground,0,0);
-    
-    for( let i = 0; i < snake.length ; i++){
-        ctx.fillStyle = ( i == 0 )? "green" : "white";
-        ctx.fillRect(snake[i].x,snake[i].y,box,box);
-        
-        ctx.strokeStyle = "red";
-        ctx.strokeRect(snake[i].x,snake[i].y,box,box);
-    }
-    
-    ctx.drawImage(foodImg, food.x, food.y);
-    
-    // old head position
-    let snakeX = snake[0].x;
-    let snakeY = snake[0].y;
-    
-    // which direction
-    if( d == "LEFT") snakeX -= box;
-    if( d == "UP") snakeY -= box;
-    if( d == "RIGHT") snakeX += box;
-    if( d == "DOWN") snakeY += box;
-    
-    // if the snake eats the food
-    if(snakeX == food.x && snakeY == food.y){
-        score++;
-        food = {
-            x : Math.floor(Math.random()*17+1) * box,
-            y : Math.floor(Math.random()*15+3) * box
-        }
-        // we don't remove the tail
-    }else{
-        // remove the tail
-        snake.pop();
-    }
-    
-    // add new Head
-    
-    let newHead = {
-        x : snakeX,
-        y : snakeY
-    }
-    
-    // game over
-    
-    if(snakeX < box || snakeX > 17 * box || snakeY < 3*box || snakeY > 17*box || collision(newHead,snake)){
-        clearInterval(game);
-    }
-    
-    snake.unshift(newHead);
-    
-    ctx.fillStyle = "white";
-    ctx.font = "45px Changa one";
-    ctx.fillText(score,2*box,1.6*box);
-}
-
-// call draw function every 100 ms
-
-let game = setInterval(draw,100);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Bind server to port
+app.listen(port, function listen() {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
